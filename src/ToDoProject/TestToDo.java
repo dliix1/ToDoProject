259b8021319dac708c41f893978571a1a3def856
Zuhdija Delic <zuka_delic@hotmail.com>
2019-07-15 17:25:01 +0200

    Initial commit

--- src/ToDoProject/TestToDo.java
+++ src/ToDoProject/TestToDo.java
@@ -14,19 +14,19 @@ public class TestToDo {
 	static ArrayList<Zadatak> zadaci = new ArrayList<>();
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		menu();
 
 	}
 
-	public static void menu(){
+	public static void menu() {
 		System.out.println("--- TODO APP ---");
 		System.out.println("Odaberite akciju: ");
 		System.out.println("1. Kreiranje racuna. ");
 		System.out.println("2. Logovanje u aplikaciju. ");
-		
+
 		int unosKorisnika = unos.nextInt();
-		
+
 		switch (unosKorisnika) {
 		case 1:
 			kreiranjeRacuna();
@@ -46,120 +46,126 @@ public class TestToDo {
 		System.out.println("2. Editovanje vec postojeceg zadatka. ");
 		System.out.println("3. Brisanje vec postojeceg zadatka. ");
 		System.out.println("4. Izlistavanje zadataka. ");
-		
+
 		int unosKorisnika = unos.nextInt();
-		
+
 		switch (unosKorisnika) {
 		case 1:
-			dodavanjeNovogZadatka(null);
+			dodavanjeNovogZadatka();
 			break;
 
-		case 2: 
+		case 2:
 			editovanjeZadataka();
 			break;
-		
-		case 3: 
+
+		case 3:
 			brisanjeZadatka();
 			break;
-			
-		case 4: 
+
+		case 4:
 			izlistavanjeZadataka();
 			break;
 		}
 
 	}
 
-	public static void kreiranjeRacuna(){
+	public static void kreiranjeRacuna() {
 		System.out.println("Unesite svoje ime: ");
 		String imeVlasnika = unos.next();
 
-		System.out.println("Unesite vas password (mora sadrzavati vise od 7 slova, i 2 broja): ");
+		System.out
+				.println("Unesite vas password (mora sadrzavati vise od 7 slova, i 2 broja): ");
 		String password = unos.next();
 
 		if (password.length() < 7) {
-			System.out.println("Password mora sadrzavati vise od 7 slova! Unesite vas password ponovo. ");
+			System.out
+					.println("Password mora sadrzavati vise od 7 slova! Unesite vas password ponovo. ");
 			password = unos.next();
 		}
 
 		new Racun(imeVlasnika, password);
-		
+
 		System.out.println("Uspjesno ste kreirali svoj racun!");
 		System.out.println();
-		
+
 		menu();
-		
-	}
-	
-	public static void racunLogin(){
-		System.out.println("Unesite vase ime: ");
-		String imeVlasnika = unos.next();
-		
-		System.out.println("Unesite vas password: ");
-		String password = unos.next();
-		
-		System.out.println("Uspjesno ste se logovali.");
-		System.out.println();
-		
-		loginMenu();
-		
+
 	}
 
-	public static void dodavanjeNovogZadatka(Date datumKreiranjaZadatka) {
+	public static void racunLogin() {
 		System.out.println("Unesite vase ime: ");
 		String imeVlasnika = unos.next();
-		
+
+		System.out.println("Unesite vas password: ");
+		String password = unos.next();
+
+		System.out.println("Uspjesno ste se logovali.");
+		System.out.println();
+
+		new Racun(imeVlasnika, password);
+
+		loginMenu();
+
+	}
+
+	public static void dodavanjeNovogZadatka() {
+		System.out.println("Unesite vase ime: ");
+		String imeVlasnika = unos.next();
+
 		System.out.println("Unesite opis zadatka: ");
 		String opisZadatka = unos.next();
-		
+
 		System.out.println("Unesite vrijeme kreiranja zadatka (dd-MM-yyyy)");
 		DateFormat noviDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		Date datumKreiranjaZadatka = null;
 		String datumKreiranja = noviDateFormat.format(datumKreiranjaZadatka);
 
 		boolean statusZadatka = false;
-		
-		Zadatak noviZadatak = new Zadatak (opisZadatka, statusZadatka, imeVlasnika, datumKreiranjaZadatka);
-		
+
+		Zadatak noviZadatak = new Zadatak(opisZadatka, statusZadatka,
+				imeVlasnika, datumKreiranjaZadatka);
+
 		zadaci.add(noviZadatak);
-		
+
 		loginMenu();
-		
+
 	}
-	
-	public static void editovanjeZadataka(){
+
+	public static void editovanjeZadataka() {
 		System.out.println("Unesite broj zadatka: ");
 		int brojZadatka = unos.nextInt();
-		
+
 		System.out.println("Unesite novu deskripiciju zadatka: ");
 		String editovaniTekst = unos.next();
-		
+
 		UpravljanjeZadatkom upravljanje = new UpravljanjeZadatkom();
-		
+
 		upravljanje.editovanjeZadatka(brojZadatka, editovaniTekst, zadaci);
-		
+
 		System.out.println("Zadatak je uspjesno editovan!");
-		
+
 		loginMenu();
 	}
-	
-	public static void brisanjeZadatka(){
+
+	public static void brisanjeZadatka() {
 		System.out.println("Unesite broj zadatka: ");
 		int brojZadatka = unos.nextInt();
-		
+
 		UpravljanjeZadatkom upravljanje = new UpravljanjeZadatkom();
 		upravljanje.brisanjeZadatka(brojZadatka, zadaci);
-		
+
 		System.out.println("Zadatak je uspjesno izbrisan!");
-		
+
 		loginMenu();
 	}
-	
-	public static void izlistavanjeZadataka(){
-		if(zadaci.size() != 0){
+
+	public static void izlistavanjeZadataka() {
+		if (zadaci.size() != 0) {
 			UpravljanjeZadatkom upravljanje = new UpravljanjeZadatkom();
 			upravljanje.izlistavanjeZadataka(zadaci);
-			
+
 			loginMenu();
 		}
 	}
-	
+
 }
